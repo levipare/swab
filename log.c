df5d8845aa3a245d7e319efc6aeaa29e4139d9e6
Levi Pare <levipare@gmail.com>
2025-04-13 16:09:20 -0400

    log to stderr instead of stdout

--- log.c
+++ log.c
@@ -12,10 +12,10 @@ void _log(int level, const char *file, int line, const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
 
-    fprintf(stdout, "\033[1m%s:%d:\033[0m \033[1;%dm%s:\033[0m ", file, line,
+    fprintf(stderr, "\033[1m%s:%d:\033[0m \033[1;%dm%s:\033[0m ", file, line,
             colors[level], levels[level]);
-    vfprintf(stdout, fmt, args);
-    fputc('\n', stdout);
+    vfprintf(stderr, fmt, args);
+    fputc('\n', stderr);
 
     va_end(args);
 
